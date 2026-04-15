c5df92b320810d8bc8295843c2bae2f47937872c
Regina <A00841923@tec.mx>
2026-04-15 00:14:15 -0600

    Frontend listo

---
+++ public/js/game.js
@@ -0,0 +1,30 @@
+const game = document.getElementById("gameScreen");
+const btn = document.getElementById("fsBtn");
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        // Entrar a fullscreen
+        if (game.requestFullscreen) {
+            game.requestFullscreen();
+        } else if (game.webkitRequestFullscreen) {
+            game.webkitRequestFullscreen();
+        }
+    } else {
+        // Salir de fullscreen
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
+// Cambiar icono
+document.addEventListener("fullscreenchange", () => {
+    const icon = btn.querySelector("i");
+    if (document.fullscreenElement) {
+        icon.classList.replace("bi-fullscreen", "bi-fullscreen-exit");
+    } else {
+        icon.classList.replace("bi-fullscreen-exit", "bi-fullscreen");
+    }
+});
