1f9825b20b478d616199ae4e6e9add868ec66a4c
Regina <A00841923@tec.mx>
2026-04-21 17:59:30 -0600

    .

--- public/js/game.js
+++ public/js/game.js
@@ -1,35 +1,6 @@
 const game = document.getElementById("gameScreen");
-const btn = document.getElementById("fsBtn");
 const gameFrame = document.getElementById("game-frame");
 
 if (sessionStorage.getItem("id")) {
         gameFrame.src = "OTMANIA_GAME/index.html?id=" + sessionStorage.getItem("id");
     }
-
-function toggleFullScreen() {
-    if (!document.fullscreenElement) {
-        // Entrar a fullscreen
-        if (game.requestFullscreen) {
-            game.requestFullscreen();
-        } else if (game.webkitRequestFullscreen) {
-            game.webkitRequestFullscreen();
-        }
-    } else {
-        // Salir de fullscreen
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        }
-    }
-}
-
-// Cambiar icono
-document.addEventListener("fullscreenchange", () => {
-    const icon = btn.querySelector("i");
-    if (document.fullscreenElement) {
-        icon.classList.replace("bi-fullscreen", "bi-fullscreen-exit");
-    } else {
-        icon.classList.replace("bi-fullscreen-exit", "bi-fullscreen");
-    }
-});
